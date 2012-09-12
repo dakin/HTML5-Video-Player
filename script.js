21d7d3b3d511f4283a0d3d63a098a1304a1f5123
dakin <andy.dakin@hudl.com>
2012-09-11 21:02:46 -0500

    Toby's Shiz

---
+++ script.js
@@ -0,0 +1,24 @@
+// ensure the web page (DOM) has loaded
+document.addEventListener("DOMContentLoaded", function () {
+
+   // Create a popcorn instance by calling Popcorn("#id-of-my-video")
+   var pop = Popcorn("#hudlVideoPlayer");
+
+   // add a footnote at 2 seconds, and remove it at 6 seconds
+   pop.footnote({
+     start: 2,
+     end: 6,
+     text: "Top Play!",
+     target: "annotation-text-1"
+   });
+
+   // play the video right away
+   pop.play();
+
+}, false);
+
+$(document).ready(function() {
+
+	$( "#annotation-text-1" ).draggable();
+
+});
